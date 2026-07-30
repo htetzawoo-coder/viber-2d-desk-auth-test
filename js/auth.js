21624d7af46206a628d41a51f6124ca2ae8263a7
htetzawoo-coder <htetzawoohzo99@gmail.com>
2026-07-31 02:12:42 +0630

    Update AUTH_STAGE_VERSION to 5.0A.2

--- js/auth.js
+++ js/auth.js
@@ -21,7 +21,7 @@ function applyAuthLanguage(lang){
 function setAuthLanguage(lang){applyAuthLanguage(lang);window.v2dRefreshPwaUi?.();}
 function authMsg(my,en){return authLanguage()==='en'?en:my;}
 
-const AUTH_STAGE_VERSION = "5.0A.1";
+const AUTH_STAGE_VERSION = "5.0A.2";
 let v2dAppScriptLoaded = false;
 
 function authEl(id){ return document.getElementById(id); }
